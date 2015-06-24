6f7c58e079b5d18478d0e07129132722666229c1
bestmomo <colorstonic@free.fr>
2015-06-24 17:27:17 +0200

    Add route and controller for home

---
+++ src/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bestmomo\Scafold\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+	/**
+	 * Show the application dashboard to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('home');
+	}
+}
--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+Route::get('home', '\Bestmomo\Scafold\Http\Controllers\HomeController@index');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
