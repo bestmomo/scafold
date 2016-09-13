7bafc1c2eb58031597cebd009bbcc6fd3d96d2fe
Marcus Vinícius Campos <marcus-campos@users.noreply.github.com>
2016-09-13 01:02:17 -0300

    Bugfix routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,18 +3,18 @@
 Route::get('home', '\MarcusCampos\Scafold\Http\Controllers\HomeController@index');
 
 // Authentication routes...
-Route::get('auth/login', 'Auth\LoginController@getLogin');
-Route::post('auth/login', 'Auth\LoginController@postLogin');
-Route::get('auth/logout', 'Auth\LoginController@getLogout');
+Route::get('auth/login', 'Auth\LoginController@login');
+Route::post('auth/login', 'Auth\LoginController@login');
+Route::get('auth/logout', 'Auth\LoginController@logout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\RegisterController@getRegister');
-Route::post('auth/register', 'Auth\RegisterController@postRegister');
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register', 'Auth\RegisterController@register');
 
 // Password reset link request routes...
-Route::get('password/email', 'Auth\ForgotPasswordController@getEmail');
-Route::post('password/email', 'Auth\ForgotPasswordController@postEmail');
+Route::get('password/email', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
 
 // Password reset routes...
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@getReset');
-Route::post('password/reset', 'Auth\ResetPasswordController@postReset');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
